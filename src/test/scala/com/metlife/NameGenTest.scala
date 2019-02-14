2fea27585ecb8d903da1fb03a7390e08429af2e4
dpeechara <dhiraj.peechara@gmail.com>
2019-02-14 11:06:34 -0500

    all tests passed

--- src/test/scala/com/metlife/NameGenTest.scala
+++ src/test/scala/com/metlife/NameGenTest.scala
@@ -1,6 +1,7 @@
 package com.metlife.datagen
 
 import java.io.File
+import java.nio.file.Paths
 
 import com.holdenkarau.spark.testing.SharedSparkContext
 import com.metlife.mask.NameGen
@@ -11,8 +12,11 @@ class NameGenTest extends FunSuite with SharedSparkContext {
 
   test("test NameGenTest") {
     val sqlContext = new SQLContext(sc)
-    val url = classOf[NameGenTest].getClassLoader.getResource("flow2.csv")
-    val f = new File(url.toURI().getPath+"/../names.parquet")
+    import java.nio.file.Paths
+    val platformIndependentPath = Paths.get(classOf[NameGenTest].getClassLoader.getResource("flow2.csv").toURI).toString
+
+    val parent = Paths.get(platformIndependentPath).getParent
+    val f = new File(parent.getParent+"/names.parquet")
     println(" absolute path : "+f.getAbsolutePath)
     NameGen.main(sqlContext.sparkSession,f.getAbsolutePath)
     //TODO ensure names are generated.
