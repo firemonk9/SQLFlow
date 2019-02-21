f4841fe1259c16f71c06af995da1d1c2fbaa02ab
dpeechara <dhiraj.peechara@gmail.com>
2019-02-21 11:36:04 -0500

    improved test cases

---
+++ src/main/scala/com/metlife/loadtest/LoadTester.scala
@@ -0,0 +1,36 @@
+package com.metlife.loadtest
+
+import com.metlife.mask.Mask
+import org.apache.spark.sql.SparkSession
+
+
+
+object LoadTester {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().appName("SQLWorkFlow").enableHiveSupport().getOrCreate()
+    val globalName = if(args.length ==1) args(0) else "SSN1"
+    val start = if(args.length == 2) args(1).toInt else 1
+    val end = if(args.length ==3) args(2).toInt else 1000000
+    val numDigits = if(args.length ==3) args(2).toInt else 9
+
+
+    createDF(spark,globalName,start,end,numDigits)
+  }
+
+  def createDF(spark:SparkSession,globalName:String ,begin:Int, end:Int,numDigits:Int):Int={
+
+    import org.apache.spark.sql.types._
+
+    val df = spark.range(begin,end).toDF("ssn1")
+    val df1 = df.withColumn("name",df("ssn1").cast(StringType))
+    println(df1.count())
+    df1.count().toInt
+    val beginT = System.currentTimeMillis()
+    Mask.maskColumn(df1,"ssn1",globalName,Mask.REAL_NUMBER,true,numDigits)
+    val endT = System.currentTimeMillis()
+    println("Total time taken is "+(endT-beginT)/1000+" secs")
+    10
+  }
+
+}
--- src/test/scala/com/metlife/mask/MaskTest.scala
+++ src/test/scala/com/metlife/mask/MaskTest.scala
@@ -1,6 +1,7 @@
 package com.metlife.mask
 
 import com.holdenkarau.spark.testing.SharedSparkContext
+import com.metlife.loadtest.LoadTester
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import org.apache.spark.sql._
 
@@ -13,6 +14,16 @@ class MaskTest extends FunSuite with SharedSparkContext  {
   val sampleData = List(Person("John",123456,"abc Cary, nc"),Person("Rob",123457,"abc Cary, nc"),Person("Mary",123458,"abc Cary, nc"))
   val sampleDataNew = List(Person("John",123456,"abc Cary, nc"),Person("Harry",123434,"abc Cary, nc"))
 
+  val sampleDataNeg = List(Person("John",123456,"abc Cary, nc"),Person("Rob",123457,"abc Cary, nc"),Person("Mary",123458,"abc Cary, nc"),Person("GREEN",-123458,"abc Cary, nc"))
+  val sampleDataNewNeg = List(Person("John",123456,"abc Cary, nc"),Person("Harry",123434,"abc Cary, nc"))
+
+
+  test("loadtest 1"){
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
+    val spark = sqlContext.sparkSession
+    assert(LoadTester.createDF(spark,10,100) == 90)
+  }
 
   test("testMaskNumber") {
     val sqlContext = new SQLContext(sc)
@@ -31,5 +42,27 @@ class MaskTest extends FunSuite with SharedSparkContext  {
     assert(ndf.count() == sampleFNew.count())
 
   }
+
+//TODO yet to implement
+//  test("testMaskNumber with negative values") {
+//    val sqlContext = new SQLContext(sc)
+//    import sqlContext.implicits._
+//    val spark = sqlContext.sparkSession
+//    spark.sql("create database mask_db")
+//    val sampleF = spark.sparkContext.parallelize(sampleDataNeg).toDF
+//    val sampleFNew = spark.sparkContext.parallelize(sampleDataNewNeg).toDF
+//    val edf = Mask.maskColumn(sampleF,"ssn","SSN",Mask.REAL_NUMBER,true,9,true)
+//    edf.show()
+//
+//    val ndf = Mask.maskColumn(sampleFNew,"ssn","SSN",Mask.REAL_NUMBER,true,9,true)
+//    ndf.show()
+//    assert(edf.filter(edf("name") === "John").head() == ndf.filter(ndf("name") === "John").head())
+//    assert(edf.filter(edf("name") === "GREEN").head() == sampleF.filter(ndf("name") === "GREEN").head())
+//
+//    assert(edf.count() == sampleF.count())
+//    assert(ndf.count() == sampleFNew.count())
+//
+//  }
+
 
 }
